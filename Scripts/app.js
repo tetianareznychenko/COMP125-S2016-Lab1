80fa5d165714d533717c16de785fd8fea18be3f5
tetianareznychenko <tatiana.reznuchenko@gmail.com>
2016-05-11 16:34:04 -0400

    Lab1

---
+++ Scripts/app.js
@@ -0,0 +1,20 @@
+//custom javascript
+
+//self-executing anonymous function 
+    
+// IIFI - Immediately Invoked Function Expression 
+
+(function(){
+    
+    
+    console.log("App Started...");
+
+// variable declaration and Initialization 
+
+var number = 0;
+number = Math.floor((Math.random()*10)+1); 
+console.log(number);
+})()
+
+
+
